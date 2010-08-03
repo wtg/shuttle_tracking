fe4cb578b65e59a4c5360cb232511063be132979
Brian M <michab3@rpi.edu>
2010-08-03 17:49:59 -0400

    Renaming the vehicle fixtures.

--- test/functional/vehicles_controller_test.rb
+++ test/functional/vehicles_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class VehiclesControllerTest < ActionController::TestCase
   setup do
-    @vehicle = vehicles(:one)
+    @vehicle = vehicles(:bus_one)
   end
 
   test "should get index" do
