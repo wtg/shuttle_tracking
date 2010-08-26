252d74121f7193f625ec37744397e5917d74a66a
Brian M <michab3@rpi.edu>
2010-08-26 09:39:09 -0400

    Removing positions model.  This will be renamed to update in a bit.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,2 @@
 class Status < ActiveRecord::Base
-  has_many :positions, :foreign_key => :code
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,10 +2,7 @@ class Vehicle < ActiveRecord::Base
 
   # Every vehicle that has an identifier must have a unique one.
   validates :identifier, :uniqueness => true, :allow_nil => true, :allow_blank => true
-  has_many :positions
   
-  def current_position
-    self.positions.find(:first, :order => 'updated_at DESC')
-  end
+  #ToDo: Add current_position replacement here.
     
 end
