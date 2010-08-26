3d8f55ad6c40bc2d2697db692404f483ba9d9528
Brian M <michab3@rpi.edu>
2010-08-26 10:20:08 -0400

    I like to use references.

--- db/migrate/20100819030542_create_routes_stops.rb
+++ db/migrate/20100819030542_create_routes_stops.rb
@@ -1,8 +1,8 @@
 class CreateRoutesStops < ActiveRecord::Migration
   def self.up
     create_table :routes_stops, :id => false do |t|
-    	t.integer :stop_id
-    	t.integer :route_id
+    	t.references :stop
+    	t.references :route
   	end
   end
 
