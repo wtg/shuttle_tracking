d0cf7053dd1c499cb1de64c3b6ed1f68898c6901
Brian M <michab3@rpi.edu>
2010-11-18 11:03:24 -0500

    Hex colors were coming back as nil instead of a string.  Weird.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   # Converts the format from #RRGGBB to AABBGGRR.
   # The alpha channel defaults to ff.
   def to_kmlcolor(color="#000000", alpha = "ff")
-    alpha + color[5,3] + color[3,2] + color[1,2]
+    alpha + color[5,3].to_s + color[3,2].to_s + color[1,2].to_s
   end
 
 end
